876de7e660ae78a64c2ff16efe3362571563f840
Amir Ojha <amir.ojha@team.aussiebroadband.com.au>
2023-01-29 19:36:25 +1030

    fixes pages

--- resources/views/frontend/components/news/order-eight.blade.php
+++ resources/views/frontend/components/news/order-eight.blade.php
@@ -6,14 +6,14 @@
     @foreach($order8News as $key=> $news)
         <div class="bl-newsPost bl-newsPost--small">
             <figure class="post_img">
-                <a href="{{route('np.category.news.show',['category'=>$news->category_slug,'c_id'=>$news->c_id])}}">
+                <a href="{{route('category.news.show',['category'=>$news->category_slug,'c_id'=>$news->c_id])}}">
                     <img src="{{$news->image}}"
                          alt="{!! $news->title !!} "/>
                 </a>
             </figure>
             <div class="post_content">
                 <h5 class="post_title">
-                    <a href="{{route('np.category.news.show',['category'=>$news->category_slug,'c_id'=>$news->c_id])}}">
+                    <a href="{{route('category.news.show',['category'=>$news->category_slug,'c_id'=>$news->c_id])}}">
                         {!! $news->title !!}
                     </a>
                 </h5>
