6a902182778a2c7544aa1a537b9f8607b36f5fe2
Amir Ojha <amir.ojha@team.aussiebroadband.com.au>
2023-02-01 08:02:12 +1030

    fixes

--- resources/views/frontend/category/index.blade.php
+++ resources/views/frontend/category/index.blade.php
@@ -79,7 +79,11 @@
                                         </h5>
                                         <span
                                             class="post_source">
-                                            {{$news->guest ?? $news->reporter->name}}
+                                            @if($news->guest)
+                                                {{$news->guest}}
+                                            @elseif($news->reporter)
+                                                {{$news->reporter->name}}
+                                            @endif
                                             {{$news->date_line ? '- '. $news->date_line : ''}}
                                         </span>
                                     </div>
