48b106a23d97a3c42f78da365cc5799d87375d1f
Amir Ojha <amir.ojha@team.aussiebroadband.com.au>
2023-03-18 14:51:45 +1030

    remove listner

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,9 +19,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        ImageWasUploaded::class => [
-            ResizeUploadedImage::class
-        ]
+//        ImageWasUploaded::class => [
+//            ResizeUploadedImage::class
+//        ]
     ];
 
     /**
