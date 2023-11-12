2be9f5ad98acff376386a26d8ee27dee7217cef8
Amir Ojha <amir.ojha@team.aussiebroadband.com.au>
2023-11-12 16:47:59 +1100

    design change.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,6 @@ class HomeController extends Controller
 
     public function __construct(NewsRepository $newsRepository, CategoryRepository $categoryRepository)
     {
-        dd('dddd');
         $this->newsRepository = $newsRepository;
         $this->categoryRepository = $categoryRepository;
     }
