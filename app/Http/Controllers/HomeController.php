4092735dee79635a83b1c8940297d3739d38bd2e
Pushpa Raj Dangi <dcpushparaj@gmail.com>
2024-06-19 08:05:15 +0545

    video

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,7 +34,7 @@ class HomeController extends Controller
         $otherNews = $this->newsRepository->getOthersNews();
         $trendingNews = $otherNews->where('category_slug', 'trending');
         $breakingNews = $otherNews->where('category_slug', 'breaking');
-        $videoNews = $otherNews->where('category_slug', 'video-report');
+        $videoNews = $otherNews->where('category_slug', 'video');
         $blSpecialNews = $otherNews->where('category_slug', 'special');
         $anchorNews = $otherNews->where('category_slug', 'anchor');
 
