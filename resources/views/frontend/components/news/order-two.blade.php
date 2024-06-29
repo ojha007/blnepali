387cf6cb30cde4cc5b755f34ccbf3bd685a3a000
Pushpa Raj Dangi <dcpushparaj@gmail.com>
2024-06-29 16:29:49 +0545

    kala

--- resources/views/frontend/components/news/order-two.blade.php
+++ resources/views/frontend/components/news/order-two.blade.php
@@ -8,29 +8,31 @@
     </div>
 
     @foreach($order2News as $key => $news)
-        <div class="media d-flex mb-4 gap-4 align-items-center">
-            <a class="d-block w-100" href="{{ route('showDetail', ['c_id' => $news->c_id]) }}">
-                <img src="{{getResizeImage($news->image)}}"
-                     class="img-fluid"
-                     alt="{{$news->image_alt ?? $news->image_description ?? ''}}"/>
-            </a>
-            <div class="media-body">
-                <h3 class="fw-bold medium-title fs-5">
-                    <a href="{{ route('showDetail', ['c_id' => $news->c_id]) }}">
-                        {!! $news->title !!}
-                    </a>
-                </h3>
-                <p class="post-description-sm d-none d-md-none d-lg-block">
-                    {!! \Illuminate\Support\Str::limit($news->short_description) !!}
-                </p>
-                <h5
-                        class="text-muted source fs-6 fw-bold text-info"
-                >
-                    {{$news->guest ?? $news->reporter_name ?? '' }}
-                    {{$news->date_line ? '-' .$news->date_line  :''}}
-                </h5>
+        <a class="d-block w-100" href="{{ route('showDetail', ['c_id' => $news->c_id]) }}">
+            <div class="row mb-4">
+                <div class="col-xs-12 col-md-4">
+                    <img src="{{getResizeImage($news->image)}}"
+                    class="img-fluid"
+                    alt="{{$news->image_alt ?? $news->image_description ?? ''}}"/>
+                </div>
+                   
+                <div class="col-sm-12 col-md-8 media-body">
+                    <h3 class="fw-bold medium-title fs-5">
+                           {!! $news->title !!}
+                    </h3>
+                    <p class="post-description-sm d-none d-md-none d-lg-block">
+                        {!! \Illuminate\Support\Str::limit($news->short_description) !!}
+                    </p>
+                    <h5
+                            class="text-muted source fs-6 fw-bold text-info"
+                    >
+                        {{$news->guest ?? $news->reporter_name ?? '' }}
+                        {{$news->date_line ? '-' .$news->date_line  :''}}
+                    </h5>
+                </div>
             </div>
-        </div>
+        </a>
+
     @endforeach
 @endisset
 
