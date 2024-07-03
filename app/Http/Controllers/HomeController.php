82f2bc096b46c11cab0470e158476aa2a7d2577f
pushpa-raj-dangi <pushpa-raj-dangi@users.noreply.github.com>
2024-07-03 14:26:03 +0000

    Fixes coding style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ use App\Repositories\CategoryRepository;
 use App\Repositories\NewsRepository;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
-use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
@@ -17,8 +16,7 @@ class HomeController extends Controller
     public function __construct(
         protected NewsRepository $newsRepository,
         protected CategoryRepository $categoryRepository
-    ) {
-    }
+    ) {}
 
     public function index(): Renderable
     {
@@ -50,7 +48,7 @@ class HomeController extends Controller
         $jiwansaili = $allNews->where('category_id', 72)->take(3)->values();
 
         return view(
-            $this->viewPath . 'index',
+            $this->viewPath.'index',
             compact(
                 'order1News',
                 'trendingNews',
@@ -82,7 +80,7 @@ class HomeController extends Controller
         $category = Category::whereSlug($categorySlug)
             ->select('id')
             ->first();
-        if (!$category) {
+        if (! $category) {
             return redirect('/');
         }
 
@@ -127,7 +125,7 @@ class HomeController extends Controller
         $breakingNews = $otherNews->where('category_slug', 'breaking');
 
         return view(
-            $this->viewPath . 'news-detail',
+            $this->viewPath.'news-detail',
             compact(
                 'news',
                 'blSpecialNews',
@@ -179,7 +177,7 @@ class HomeController extends Controller
         $breakingNews = $otherNews->where('category_slug', 'breaking');
 
         return view(
-            $this->viewPath . 'news-detail',
+            $this->viewPath.'news-detail',
             compact(
                 'news',
                 'blSpecialNews',
