7ea692cc44e01a255c82ffd89f4283c75dbfc183
Pushpa Raj Dangi <dcpushparaj@gmail.com>
2024-07-21 16:53:11 +0545

    new changes

--- resources/views/frontend/components/news/order-one.blade.php
+++ resources/views/frontend/components/news/order-one.blade.php
@@ -24,7 +24,7 @@
                     </h1>
                     <span class="text-muted fs-6 fw-bold px-3 text-info">{!! $order1First->reporter_name !!}</span>
                     <p class="post-description-sm p-3">
-                        {!! $order1First->short_description !!}
+                        {!! $order1First->sub_title !!}
 
                     </p>
                 </a>
