359e05468fa7b58195483baeeb124a00873e7e5b
ojha007 <ojha007@users.noreply.github.com>
2024-07-23 05:06:37 +0000

    Fixes coding style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        LogViewer::auth(fn($request) => true);
+        LogViewer::auth(fn ($request) => true);
         View::composer('*', HeaderCategoryComposer::class);
     }
 }
