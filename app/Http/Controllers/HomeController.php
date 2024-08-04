1f6348c39d027578b72ba6b3046b94625b3bb22e
ojha007 <ojhaamir007@gmail.com>
2024-08-04 23:14:57 +1000

    fixes non-news issues

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -102,110 +102,18 @@ class HomeController extends Controller
         $blSpecialNews = $nonCategoryNews->where('category', 'special');
         $breakingNews = $nonCategoryNews->where('category', 'breaking');
 
-        $otherSamachar = [];
         $similarNews = $this->newsRepository->getSimilarNewsBySlug($news);
 
         return view(
-            'frontend.news-detail',
+            'frontend.news.show',
             compact(
                 'news',
                 'blSpecialNews',
                 'trendingNews',
                 'sameCategoryNews',
                 'breakingNews',
-                'otherSamachar',
                 'similarNews',
             )
         );
-    }
-
-    public function showDetail($cId)
-    {
-
-        $cacheKey = News::getCacheKey($cId);
-
-        //        $allNews = Cache::remember($cacheKey, 1800, function () use ($cId, $category) {
-
-        $allNews = News::query()
-            ->with(['category:name,id,slug', 'reporter:name,id,image'])
-            ->select([
-                'title',
-                'short_description',
-                'guest_id',
-                'image_description',
-                'description',
-                'video_url',
-                'date_line',
-                'publish_date',
-                'id',
-                'c_id',
-                'image',
-                'image_alt',
-                'category_id',
-                'reporter_id',
-            ])
-            ->where('c_id', '=', $cId)
-            ->orderByDesc('publish_date')
-            ->get();
-        //        });
-
-        $news = $allNews->where('c_id', '=', $cId)->first();
-
-        $news->increment('view_count');
-
-        $sameCategoryNews = $this->newsRepository->sameCategoryNewsQuery($news->category_id)
-            ->where('c_id', '!=', $cId)
-            ->take(3)
-            ->get();
-
-        //  yo pani DB::table('news')
-        //         ->select('id', 'title')
-        //         ->where('slug', 'like', '%' . $slug . '%')
-        //         ->where('id', '!=', $id)
-        //         ->whereNull('deleted_at')
-        //         ->whereNotNull('slug')
-        //         ->inRandomOrder()
-        //         ->take(5)
-        //         ->get();
-
-        $otherNews = $this->newsRepository->getIndexPageNonCategoryNews();
-        $trendingNews = $otherNews->where('category_slug', 'trending');
-        $blSpecialNews = $otherNews->where('category_slug', 'special');
-        $breakingNews = $otherNews->where('category_slug', 'breaking');
-        $otherSamachar = News::query()
-            ->with(['category:name,id,slug', 'reporter:name,id,image'])
-            ->select([
-                'title',
-                'short_description',
-                'guest_id',
-                'image_description',
-                'description',
-                'video_url',
-                'date_line',
-                'publish_date',
-                'id',
-                'c_id',
-                'image',
-                'image_alt',
-                'category_id',
-                'reporter_id',
-            ])
-            ->orderByDesc('publish_date')
-            ->skip(1)
-            ->take(6)
-            ->get();
-
-        return view(
-            $this->viewPath . 'news-detail',
-            compact(
-                'news',
-                'blSpecialNews',
-                'trendingNews',
-                'sameCategoryNews',
-                'breakingNews',
-                'otherSamachar'
-            )
-        );
-
     }
 }
