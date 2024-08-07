b4f89515a84bdd1e716fc246ef6e50f5aa2d1361
ojha007 <ojhaamir007@gmail.com>
2024-08-07 12:56:15 +1000

    fixes css.

--- resources/views/frontend/components/news/ghumphir.blade.php
+++ resources/views/frontend/components/news/ghumphir.blade.php
@@ -6,30 +6,31 @@
             </h5>
         </div>
 
-        {{-- Main News Section (col-md-8) --}}
         <div class="col-md-8">
             @if($ghumphir->isNotEmpty())
                 @php $mainNews = $ghumphir->first(); @endphp
-                <a href="{{ route('category.news.show', ['category' => $mainNews->category_slug, 'c_id' => $mainNews->c_id]) }}" class="card a-hover h-100 border-0">
-                    <img class="card-img h-100" src="{{ getResizeImage($mainNews->image) }}" alt="{{ $mainNews->image_alt }}">
+                <a href="{{ route('category.news.show', [$mainNews->category_slug, $mainNews->c_id]) }}"
+                   class="card a-hover h-100 border-0">
+                    <img class="card-img h-100" src="{{ getResizeImage($mainNews->image) }}"
+                         alt="{{ $mainNews->image_alt }}">
                     <div class="card-img-overlay bg-gradient-primary top-50 p-3 mt-5 text-white bottom-0">
-                        <h2 class="card-title fw-bold fs-3 overlay-postition-bttom">
-                            {{ $mainNews->title }}
+                        <h2 class="card-title fw-bold fs-3">
+                            {!! $mainNews->title !!}
                         </h2>
                     </div>
                 </a>
             @endif
         </div>
 
-        {{-- Side News Section (col-md-4) --}}
         <div class="col-md-4">
             <div class="row g-2 h-100 ghumphir-side">
-                @foreach($ghumphir->take(2) as $news)
+                @foreach($ghumphir->skip(1) as $news)
                     <div class="col-md-12 h-50">
-                        <a href="{{ route('showDetail', ['c_id' => $news->c_id]) }}" class="card d-block h-100 border-0">
+                        <a href="{{ route('showDetail', [$news->category_slug, $news->c_id]) }}"
+                           class="card d-block h-100 border-0">
                             <img class="card-img" src="{{ $news->image }}" alt="{{ $news->image_alt }}">
-                            <div class="card-img-overlay bg-gradient-primary overlaypostition text-white bottom-0">
-                                <h2 class="card-title fw-bold fs-4 overlay-postition-bttom">
+                            <div class="card-img-overlay bg-gradient-primary text-white bottom-0">
+                                <h2 class="card-title fw-bold fs-4">
                                     {!! $news->title !!}
                                 </h2>
                             </div>
